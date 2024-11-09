0ba52f41a31468faebf4dbba1444815526d70da4
ryosei15 <c1107582@planet.kanazawa-it.ac.jp>
2024-11-10 02:20:08 +0900

    プレイヤーステータス画面の表示、メッセージの表示

--- mon/app.py
+++ mon/app.py
@@ -1,52 +1,55 @@
 from fastapi import FastAPI, Request, Form, Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-from danjon import playerpm,battle
-import random
+from danjon import playerpm, monsterbox, battle
 import uvicorn
 
 app = FastAPI()
 templates = Jinja2Templates(directory="mon/templates")
 
-player_data = {
-    "username": None,
-    "level": 1,
-    "hp": 0,
-    "mp": 0,
-}
+game_state = {"player_data": None, "monster_data": None, "initial_appearance": True}
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("web.html", {"request": request, "player": player_data})
+    return templates.TemplateResponse("home.html", {"request": request})
 
-@app.post("/player_status")
+@app.post("/player_status", response_class=HTMLResponse)
 async def player_status(request: Request, username: str = Form(...)):
-    player_data["username"] = username
-    player_data["level"] = 1
-    player_data["hp"] = random.randint(50, 100)
-    player_data["mp"] = random.randint(70, 100)
-    return templates.TemplateResponse("web.html", {"request": request, "player": player_data})
+    game_state["player_data"] = playerpm()
+    game_state["player_data"][0] = username
+    player_data = {"username": username, 
+                   "level": game_state["player_data"][1],
+                   "hp": game_state["player_data"][2], 
+                   "mp": game_state["player_data"][4]}
+    return templates.TemplateResponse("home.html", {"request": request, "player": player_data})
 
-@app.get("/start_game", response_class=HTMLResponse)
+@app.post("/start_game", response_class=HTMLResponse)
 async def start_game(request: Request):
-    # バトル画面に移行
-    result = playerpm()  # モンスター出現の初期メッセージ
-    return templates.TemplateResponse("battle.html", {"request": request, "player": player_data, "result": result})
+    game_state["monster_data"], game_state["player_data"] = monsterbox(game_state["player_data"])
+    game_state["initial_appearance"] = True  # 初回のモンスター出現時のみTrue
+    monster_data = {"name": game_state["monster_data"][0], 
+                    "hp": game_state["monster_data"][1]}
+    return templates.TemplateResponse("battle.html", {"request": request, "player": game_state["player_data"], "monster": monster_data, "initial_appearance": game_state["initial_appearance"]})
 
-@app.get("/battle_action")
-async def battle_action(action: str = Query(...)):
-    # 各アクションの結果を生成し返す（簡略化した例）
-    if action == "attack":
-        result = "プレイヤーが攻撃しました！ モンスターに 10 のダメージ！"
-    elif action == "magic":
-        result = "プレイヤーが魔法を使いました！ モンスターに 20 のダメージ！"
-    elif action == "heal":
-        result = "プレイヤーが回復しました！ HP が 15 回復！"
-    elif action == "run":
-        result = "プレイヤーは逃げ出したが、失敗した！"
-    else:
-        result = "不明な行動です。"
-    return result
+@app.get("/battle_action", response_class=JSONResponse)
+async def battle_action(request: Request, action: str = Query(...)):
+    if game_state["monster_data"] is None:
+        return JSONResponse({"result": "新しいモンスターを生成する必要があります。"}, status_code=400)
+
+    result, updated_data = battle(game_state["monster_data"], game_state["player_data"], action)
+    game_state["monster_data"], game_state["player_data"] = updated_data
+
+    # モンスターが倒された場合、HPを None として返す
+    monster_data = {
+        "name": game_state["monster_data"][0] if game_state["monster_data"] else None,
+        "hp": game_state["monster_data"][1] if game_state["monster_data"] else None
+    }
+    player_data = {
+        "hp": game_state["player_data"][2],
+        "mp": game_state["player_data"][4]
+    }
+
+    return {"result": result, "monster": monster_data, "player": player_data}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- mon/danjon.py
+++ mon/danjon.py
@@ -2,161 +2,92 @@ import random
 import sys
 
 def playerpm():
-    player_name = "プレイヤー"  # 仮のプレイヤー名
+    player_name = "プレイヤー"
     player_level = 1
     player_hp = random.randint(50, 101)
     player_mp = random.randint(70, 101)
     count = 0
     down = 0
-    # リストに格納する
     player_data = [player_name, player_level, player_hp, player_hp, player_mp, player_mp, count, down]
     return player_data
 
-def monsterbox(player_name: str, player_level: int, player_hp: int, player_hp2: int, player_mp: int, player_mp2: int, count: int, down: int):
+def monsterbox(player_data):
+    player_name, player_level, player_hp, player_hp2, player_mp, player_mp2, count, down = player_data
     monsters = ['スライム', 'ゴブリン', 'オーク', 'ドラゴン']
-    result = f"{monsters[count]}が出現した！"
-    monster_hp = random.randint(30, 41) if count == 0 else random.randint(50, 60)  # 簡略化
-    return result + battle(monsters[count], monster_hp, player_name, player_level, player_hp, player_mp, count, down)
+    if count == 0:
+        monster_hp = random.randint(30, 41)
+    elif count == 1:
+        monster_hp = random.randint(51, 61)
+    elif count == 2:
+        monster_hp = random.randint(80, 91)
+    elif count == 3:
+        monster_hp = random.randint(150, 201)
+    monster_data = [monsters[count], monster_hp]
+    return monster_data, player_data
 
-def battle(monster_name: str, monster_hp: int, player_name: str, player_level: int, player_hp: int, player_hp2: int, player_mp: int, player_mp2: int, count: int, down: int):
-    while(monster_hp > 0):
-        player_at = int(random.randrange(10,16))  #プレイヤーの攻撃
-        player_mt = int(random.randrange(20,31))  #プレイヤーの魔法攻撃
-        player_rc = int(random.randrange(25,41))  #プレイヤーの回復
-        monster_ch = str(random.randrange(0,2))   #モンスターの選択
-        player_qh = str(random.randrange(0,11))
-        if count > 0 and down == 1: #レベルアップによる攻撃力アップ,HPとMPの回復&上限アップ
-            player_level += 1
-            player_hp = player_hp2
-            player_mp = player_mp2
-            player_hp += 10
-            player_mp += 10
-            player_hp2 = player_hp
-            player_mp2 = player_mp
-            down = 0
-        if count > 0:
-            player_at += player_level * 5
-            player_mt += player_level * 5
-        print(str(monster_name)+'のHP:' + str(monster_hp))
-        print(str(player_name) + 'のレベル:' + str(player_level))
-        print(str(player_name) + 'のHP:' + str(player_hp) + ' ' + str(player_name) + 'のMP:' + str(player_mp))
-        print('1:剣で攻撃 2:魔法で攻撃(MP20) 3:回復 4:逃げる')
-        player_ch = input()
-        if monster_name == 'スライム': #スライム出現の場合
-            monster_at = int(random.randrange(10,16))
-        elif monster_name == 'ゴブリン': #ゴブリン出現の場合
-            monster_at = int(random.randrange(20,26))
-        elif monster_name == 'オーク': #オーク出現の場合
-            monster_at = int(random.randrange(25,30))
-        elif monster_name == 'ドラゴン': #ドラゴン出現の場合
-            monster_at = int(random.randrange(30,41))
-        if player_ch == '1':   #剣で攻撃を選んだ場合
-            print(str(player_name) + 'は剣で攻撃をした')
-            if player_qh == '1': #クリティカルヒットした場合
-                print('モンスターにクリティカルヒットした')
-                player_at = int(player_at * 1.5)
-                monster_hp -= player_at
-            else: #通常の場合
-                monster_hp -= player_at
-            print(str(monster_name) + 'に' + str(player_at) + 'ダメージ与えた')
-            if monster_hp > 0:  #モンスターが倒れなかった場合
-                player_hp -= monster_at
-                print(str(monster_name) + 'が攻撃してきた')
-                print(str(player_name) + 'は' + str(monster_at) + 'ダメージ受けた')
-                if player_hp <= 0:  #プレイヤーが倒れた場合
-                    print(str(player_name) + 'は倒れた')
-                    sys.exit(0)
-        elif player_ch == '2':  #魔法で攻撃を選んだ場合
-            if player_mp > 20:
-                monster_hp -= player_mt
-                player_mp -= 20
-                print(str(player_name) + 'は魔法で攻撃をした')
-                print(str(monster_name) + 'に' + str(player_mt) + 'ダメージを与えた')
-                if monster_hp > 0:  #モンスターが倒れなかった場合
-                    player_hp -= monster_at
-                    print(str(monster_name) + 'が攻撃してきた')
-                    print(str(player_name) + 'は' + str(monster_at) + 'ダメージ受けた')
-                    if player_hp <= 0: #プレイヤーが倒れた場合
-                        print(str(player_name) + 'は倒れた')
-                        sys.exit(0)
-            else:
-                print('魔法を使うことができない')
-        elif player_ch == '3':  #回復を選んだ場合
-            player_rc2 = 0      #HPの回復量
-            player_rc3 = 0      #MPの回復量
-            player_c1 = 0       #HP回復した回数
-            player_c2 = 0       #MP回復した回数
-            if player_hp == player_hp2 and player_mp == player_mp2:
-                print(str(player_name) + 'のHPとMPはMAXである')
-                print()
-                continue
-            if player_mp < player_mp2:
-                if player_hp + player_rc <= player_hp2 and player_c1 == 0: #回復量がHPの上限以下の場合
-                    player_hp += player_rc
-                    player_c1 = 1
-                if player_mp + player_rc <= player_mp2 and player_c2 == 0: #回復量がMPの上限より以下の場合
-                    player_mp += player_rc
-                    player_c2 = 1
-                if player_hp + player_rc > player_hp2 and player_c1 == 0: #回復量がHPの上限を超えた場合
-                    player_rc2 = player_hp2 - player_hp
-                    player_hp = player_hp2
-                    player_c1 = 1
-                if player_mp + player_rc > player_mp2 and player_c2 == 0: #回復量がMPの上限を超えた場合
-                    player_rc3 = player_mp2 - player_mp
-                    player_mp = player_mp2
-                    player_c2 = 1
-                if player_rc2 == 0 and player_rc3 == 0: #回復量が両方超えなかった場合
-                    print(str(player_name) + 'の' + 'HPとMPが' + str(player_rc) + '回復した')
-                if player_rc2 != 0 and player_rc3 == 0: #回復量がHPだけ超えた場合
-                    print(str(player_name) + 'のHPが' + str(player_rc2) + ',MPが' + str(player_rc) + '回復した') 
-                if player_rc2 == 0 and player_rc3 != 0: #回復量がMPだけ超えた場合
-                    print(str(player_name) + 'のHPが' + str(player_rc) + ',MPが' + str(player_rc3) + '回復した')
-                if player_rc2 != 0 and player_rc3 != 0: #回復量が両方超えた場合
-                    print(str(player_name) + 'のHPが' + str(player_rc2) + ',MPが' + str(player_rc3) + '回復した')
-            if player_mp == player_mp2 and player_hp != player_hp2 and player_c1 == 0:
-                if player_hp + player_rc <= player_hp2: #回復量がHPの上限以下の場合
-                    player_hp += player_rc
-                elif player_hp + player_rc > player_hp2: #回復量がHPの上限を超えた場合
-                    player_rc2 = player_hp2 - player_hp
-                    player_hp = player_hp2
-                if player_rc2 == 0: #回復量がHPの上限を超えなかった場合
-                    print(str(player_name) + 'のHPが' + str(player_rc) + '回復した')
-                elif player_rc2 != 0: #回復量がHPの上限を超えた場合
-                    print(str(player_name) + 'のHPが' + str(player_rc2) + '回復した')
-            player_hp -= monster_at
-            print(str(monster_name) + 'が攻撃してきた')
-            print(str(player_name) + 'は' + str(monster_at) + 'ダメージ受けた')
-            if player_hp <= 0: #プレイヤーが倒れた場合
-                        print(str(player_name) + 'は倒れた')
-                        sys.exit(0)
-        elif player_ch == '4':  #逃げるを選んだ場合
-            print(str(player_name) + 'は逃げた')
-            if monster_ch == '0': #逃げることに成功した場合
-                print(str(monster_name) + 'から逃げることに成功した')
-                sys.exit(0)
-            elif monster_ch == '1': #逃げることに失敗した場合
-                print('しかし' + str(monster_name) + 'から逃げることができなかった')
-        elif player_ch == 'q': #qを入力した場合
-            print('ゲームを終了します')
-            sys.exit(0)
-        else: #1~4以外の文字を入力した場合
-            print('1~4の数字を入力してください')
-        if monster_hp > 0: #表示を見やすくするため
-            print()
-    print(str(monster_name) + 'を倒した')
-    print(str(player_name) + 'のレベルが1上がった' )
-    print()
-    count += 1
-    down = 1
-    if count == 4: #すべてのモンスターを倒した場合
-        print('すべてのモンスターを倒した')
-        sys.exit(0)
-    elif count < 4: #まだすべてのモンスターを倒していない場合
-        monsterbox(player_name, player_level, player_hp, player_hp2, player_mp, player_mp2, count, down)
+def convert_newlines_to_br(text):
+    return text.replace("\n", "<br>")
+
+def battle(monster_data, player_data, player_ch):
+    monster_name, monster_hp = monster_data
+    player_name, player_level, player_hp, player_hp2, player_mp, player_mp2, count, down = player_data
+    player_at = random.randint(10, 16)
+    player_mt = random.randint(20, 31)
+    player_rc = random.randint(25, 41)
+    monster_ch = random.choice(['0', '1'])
+    player_qh = random.choice(['0', '1'])
+
+    if count > 0 and down == 1:
+        player_level += 1
+        player_hp = player_hp2 + 10
+        player_mp = player_mp2 + 10
+        player_hp2, player_mp2 = player_hp, player_mp
+        down = 0
+
+    if player_ch == 'attack':
+        if player_qh == '1':
+            player_at = int(player_at * 1.5)
+        monster_hp -= player_at
+        action_result = f"{player_name}は剣で攻撃をした。\n{monster_name}に{player_at}ダメージを与えた。\n\n"
+    elif player_ch == 'magic' and player_mp >= 20:
+        monster_hp -= player_mt
+        player_mp -= 20
+        action_result = f"{player_name}は魔法で攻撃をした。\n{monster_name}に{player_mt}ダメージを与えた。\n\n"
+    elif player_ch == 'heal':
+        hp_healed = min(player_rc, player_hp2 - player_hp)
+        mp_healed = min(player_rc, player_mp2 - player_mp)
+        player_hp += hp_healed
+        player_mp += mp_healed
+        action_result = f"{player_name}はHPを{hp_healed}、MPを{mp_healed}回復した。\n\n"
+    elif player_ch == 'run' and monster_ch == '0':
+        return convert_newlines_to_br(f"{player_name}は逃げ出し、成功した。"), [None, None]
+    else:
+        action_result = f"{player_name}の行動は不明です。"
+    
+    if monster_hp > 0:
+        # モンスターが攻撃
+        monster_attack = random.randint(5, 15)  # モンスターの攻撃力
+        player_hp -= monster_attack
+        action_result += f"{monster_name}が攻撃してきた！\n{player_name}は{monster_attack}のダメージを受けた。\n\n"    
+
+    if monster_hp <= 0:
+        action_result += f"{monster_name}を倒した！ \n{player_name}のレベルが1上がった。"
+        count += 1
+        down = 1
+        player_data = [player_name, player_level + 1, player_hp, player_hp2, player_mp, player_mp2, count, down]
+        return convert_newlines_to_br(action_result), [None, player_data]
+    elif player_hp <= 0:
+        return convert_newlines_to_br(f"{player_name}は倒れた。\nゲームオーバー。"), [None, None]
+
+    monster_data = [monster_name, monster_hp]
+    player_data = [player_name, player_level, player_hp, player_hp2, player_mp, player_mp2, count, down]
+    return convert_newlines_to_br(action_result), [monster_data, player_data]
 
 def main():
     player_data = playerpm()
-    monsterbox(*player_data)
+    monster_data, player_data = monsterbox(player_data)
+    result, _ = battle(monster_data, player_data, "attack")  # 仮のテストとして"attack"を渡す
+    print(result)
 
 if __name__ == "__main__":
     main()
